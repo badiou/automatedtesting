2e845404e3c33d71f80639e719a58413b718d0bc
badiou <ourobadiou@gmail.com>
2024-07-15 20:06:20 +0200

    test with selenium

--- selenium/intro-to-selenium.py
+++ selenium/intro-to-selenium.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 # Démarrer le navigateur et naviguer vers le site web
 driver = webdriver.Chrome()
@@ -19,7 +21,9 @@ login_button = driver.find_element(By.CSS_SELECTOR, "input#login-button")
 login_button.click()
 
 # Attendre quelques secondes pour permettre le chargement de la page suivante
-driver.implicitly_wait(5)  # Attente implicite de 5 secondes
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, ".inventory_item"))
+)
 
 # Vérifier la présence des produits sur la page suivante
 try:
@@ -28,6 +32,28 @@ try:
         print(f"Nombre de produits trouvés : {len(products)}")
         for product in products:
             print(product.text)
+    else:
+        print("Aucun produit trouvé sur la page.")
+except Exception as e:
+    print(f"Une erreur s'est produite : {e}")
+
+#######################################################################################
+# Vérifier le clic sur le bouton add-to-cart-sauce-labs-backpack
+#######################################################################################
+try:
+    addcarts = driver.find_elements(By.CSS_SELECTOR, ".btn.btn_primary.btn_small.btn_inventory")
+    if addcarts:
+        print(f"Nombre de boutons 'Add to cart' : {len(addcarts)}")
+        for addcart in addcarts:
+            print(f"Bouton trouvé avec le texte : {addcart.text}")
+            addcart.click()
+            print('Clic sur le bouton avec la classe Add cart réussi')
+
+            # Attendre que le bouton se transforme en bouton "Remove"
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, ".btn_secondary.btn_small.btn_inventory"))
+            )
+            print('Le bouton s\'est transformé en bouton Remove')
     else:
         print("Aucun produit trouvé sur la page.")
 except Exception as e:
